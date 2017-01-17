1de1fd18c1113cf155ebcd69ffa6041a11c99bcc
Tamás Sallai <gsashee@gmail.com>
2017-01-17 17:06:02 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,15 @@
+return function()
+    local WIFI_SSID = "__SSID__"
+    local WIFI_PASSWORD = "__PASS__"
+    
+    wifi.setmode(wifi.STATION)
+    wifi.sta.config(WIFI_SSID, WIFI_PASSWORD)
+    
+    local update = function()
+      ...
+    end
+    
+    tmr.create():alarm(1000, tmr.ALARM_AUTO, update)
+    
+    update()
+end
