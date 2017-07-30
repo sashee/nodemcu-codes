64ffd30be71b078053904bd87c154ec9c5a8a34e
Tamás Sallai <gsashee@gmail.com>
2017-07-30 17:43:58 +0200

    Update main.lua

--- weight/main.lua
+++ weight/main.lua
@@ -1,24 +1,14 @@
 return function()
     local weight = require("weight");
     local disp = require("disp")
-    weight(function(err, tared)
-        if err then
-            print(err)
-        else
-            local update
-            update = function()
-                local res = weight(function(err, res)
-                    if err then
-                        print(err);
-                    else
-                        disp(res - tared);
-                        tmr.create():alarm(100, tmr.ALARM_SINGLE, update)
-                    end
-                end)
-            end
-            
-            update()
-        end
-    end)
-
+    local tared = weight()
+    
+    local update;
+    update = function()
+        local res = weight();
+        disp(res - tared);
+        tmr.create():alarm(100, tmr.ALARM_SINGLE, update)
+    end
+    
+    update();
 end
