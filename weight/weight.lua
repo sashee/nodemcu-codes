6af407cec0ac9f91d5b752b7a7aa0fdfa40c08ed
Tamás Sallai <gsashee@gmail.com>
2017-07-30 17:18:21 +0200

    Update weight.lua

--- weight/weight.lua
+++ weight/weight.lua
@@ -1,9 +1,11 @@
 hx711.init(6,7)
 
 return function(callback)
-    local measure = hx711.read(0);
-    local x =  measure / 395.8392;
-    local result = x>=0 and math.floor(x+0.5) or math.ceil(x-0.5);
-    
-    node.task.post(function() callback(nil, result) end);
+    xpcall(function()
+        local measure = hx711.read(0);
+        local x =  measure / 395.8392;
+        local result = x>=0 and math.floor(x+0.5) or math.ceil(x-0.5);
+        
+        node.task.post(function() callback(nil, result) end);
+    end, callback)
 end
